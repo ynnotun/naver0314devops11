9c2da11201a1732a5793a69bac7f55da1b7f072f
ynnotun <ynnotun@gmail.com>
2024-03-27 20:29:48 +0900

    20240327 남아서

--- javawork/Naver0314JavaProect/src/day0327/Ex9_VectorMunje.java
+++ javawork/Naver0314JavaProect/src/day0327/Ex9_VectorMunje.java
@@ -1,5 +1,14 @@
 package day0327;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 /*
  * 1. 학생정보추가 : 이름, 나이, 3과목 점수 입력
  * 2. 학생정보삭제 : 이름으로 찾아서 삭제(없을 경우 메시지 출력)
@@ -10,5 +19,229 @@ package day0327;
  * */
 
 public class Ex9_VectorMunje {
+	List<StudentDto> list = new ArrayList<StudentDto>();
+	Scanner sc = new Scanner(System.in);
+	static final String FILENAME = "d:/naver0314/student.txt";
+	
+
+	public Ex9_VectorMunje() {
+		studentReadFile();
+	}
+	
+	// 파일 읽기 - 처음 생성 시 자동으로 파일에서 멤버 읽어오기
+	public void studentReadFile() {
+		FileReader fr = null;
+		BufferedReader br = null;
+		try {
+			fr = new FileReader(FILENAME);
+			br = new BufferedReader(fr);
+			
+			while(true) {
+				String s = br.readLine();
+				if(s==null) break;
+				//,로 분리하기
+				String []a = s.split(",");
+				//StudentDto 생성 후 setter로 데이터 넣기
+				StudentDto dto = new StudentDto();
+				dto.setName(a[0]);
+				dto.setAge(Integer.parseInt(a[1]));
+				dto.setJava(Integer.parseInt(a[2]));
+				dto.setSpring(Integer.parseInt(a[3]));
+				dto.setHtml(Integer.parseInt(a[4]));
+				
+				//list 에 추가
+				list.add(dto);
+			}
+			System.out.println("총 "+list.size()+"명의 멤버 정보를 읽어왔어요!!");
+		} catch (FileNotFoundException e) {
+			System.out.println("읽어올 멤버 정보가 없습니다");
+		} catch (IOException e2) {
+			e2.printStackTrace();
+		} finally {
+			try {
+				br.close();
+				fr.close();
+			} catch (IOException | NullPointerException e2) {
+				e2.printStackTrace();
+			}
+		}
+	}
+	
+	// 종료시 파일 저장
+	private void studentFileSave() {
+		FileWriter fw = null;
+		try {
+			fw = new FileWriter(FILENAME);
+			for(StudentDto dto: list) {
+				String s = dto.getName()+","+dto.getAge()+","+dto.getJava()+","+dto.getSpring()+","+dto.getHtml()+"\n";
+				fw.write(s);
+			}
+			System.out.println("총 "+ list.size()+"정보를 파일에 저장하였습니다");
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				fw.close();
+			} catch (IOException | NullPointerException e) { // 여러 exception 처리
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	// 메뉴 선택
+	private int getMenu() {
+		int menu = 0;
+		System.out.println("1. 학생정보추가");
+		System.out.println("2. 학생정보삭제");
+		System.out.println("3. 전체출력");
+		System.out.println("4. 평균으로 검색");
+		System.out.println("5. 이름으로 검색");
+		System.out.println("6. 저장 후 종료");
+		System.out.println("=============");
+		
+		try {
+			menu = Integer.parseInt(sc.nextLine());			
+		} catch (NumberFormatException e) { // 문자 입력 시 발생하는 Exception
+			System.out.println("번호를 숫자로 입력해주세요");
+		}
+		return menu;
+		
+	}
+	
+	// 멤버 데이터 추가
+	public void addStudent() {
+		System.out.println("이름을 입력하세요");
+		String name = sc.nextLine();
+
+		System.out.println("나이를 입력하세요");
+		int age = Integer.parseInt(sc.nextLine());
+
+		System.out.println("자바 점수를 입력하세요");
+		int java = Integer.parseInt(sc.nextLine());
+
+		System.out.println("스프링 점수를 입력하세요");
+		int spring = Integer.parseInt(sc.nextLine());
+
+		System.out.println("HTML 점수를 입력하세요");
+		int html = Integer.parseInt(sc.nextLine());
+
+		StudentDto dto = new StudentDto(name, age, java, spring, html);
+		list.add(dto); // list에 추가
+		System.out.println(list.size()+"번째 멤버 정보를 추가하였습니다!");
+	}
+	
+	// 멤버 삭제
+	public void deleteStudent() {
+		boolean f=false;
+		System.out.println("삭제할 멤버명을 입력하세요");
+		String name=sc.nextLine();
+		for(int i=0;i<list.size();i++)
+		{
+			StudentDto dto=list.get(i);
+			if(dto.getName().equals(name))
+			{
+				f=true;
+				list.remove(i);
+				break;
+			}
+		}
+		if(f)
+			System.out.println(name+"님의 정보를 삭제하였습니다");
+		else
+			System.out.println(name+"님은 명단에 없습니다");
+	}
+	
+	// 전체 멤버 출력
+	public void writeStudent() {
+		System.out.println("\t** 전체 학생 명단 **\n");
+		System.out.println("=".repeat(70));
+		System.out.println("번호\t이름\t나이\t자바\t스프링\tHTML\t총점\t평균\t등급");
+		System.out.println("=".repeat(70));
+		
+		for(int i=0;i<list.size();i++)
+		{
+			StudentDto dto=list.get(i);
+			System.out.println(i+1+"\t"+dto.getName()+"\t"+dto.getAge()+"\t"
+			+ dto.getJava()+"\t"+dto.getSpring()+"\t"+ dto.getHtml()+"\t"+ dto.getTotal()+"\t"+ dto.getAvg()+"\t"+ dto.getGrade());
+		}
+		System.out.println("=".repeat(70));
+	}
+	
+	// 이름으로 검색
+	public void searchName() {
+		
+		System.out.println("이름을 입력하세요");
+		String name = sc.nextLine();
+		for(int i=0;i<list.size();i++)
+		{
+			StudentDto dto=list.get(i);
+			boolean f = dto.getName().startsWith(name);
+			if(f)
+			{
+				System.out.println("이름 : "+dto.getName());
+				System.out.println("나이 : "+dto.getAge());
+				System.out.println("자바 점수 : "+dto.getJava());
+				System.out.println("스프링 점수 : "+dto.getSpring());
+				System.out.println("HTML 점수 : "+dto.getHtml());
+				break;
+			} else {
+				System.out.println(name+"님은 명단에 없습니다");
+			}
+		}
+
+	}
+	
+	// 평균으로 검색
+	public void searchPhone() {
+		System.out.println("점수를 입력하세요");
+		int score = Integer.parseInt(sc.nextLine());
+		boolean f = false;
+		for(int i=0;i<list.size();i++)
+		{
+			StudentDto dto=list.get(i);
+			if(score <= dto.getAvg())
+			{
+				f = true;
+				System.out.println("이름 : "+dto.getName());
+				System.out.println("나이 : "+dto.getAge());
+				System.out.println("자바 점수 : "+dto.getJava());
+				System.out.println("스프링 점수 : "+dto.getSpring());
+				System.out.println("HTML 점수 : "+dto.getHtml());
+				break;
+			}
+		}
+		
+		if (!f) System.out.println("입력한 점수 이상의 학생을 모두 출력하였습니다");
+		else System.out.println(score+"점 이상의 평균 점수를 가진 학생은 없습니다");
+
+	}
+	
+	public static void main(String[] args) {
+		Ex9_VectorMunje ex = new Ex9_VectorMunje();
+		
+		while(true) {
+			int menu = ex.getMenu();
+			switch(menu) {
+			case 1:
+				ex.addStudent(); break;
+			case 2:
+				ex.deleteStudent(); break;
+			case 3:
+				ex.writeStudent(); break;
+			case 4:
+				ex.searchPhone(); break;
+			case 5:
+				ex.searchName(); break;
+			case 6:
+				ex.studentFileSave();
+				System.out.println("** 멤버 정보를 저장 후 종료합니다 **");
+				System.exit(0); // 종료
+				break;
+			default:
+				System.out.println("잘못된 번호 입니다");
+			}
+			System.out.println();
+		}
+	}
 
 }
--- javawork/Naver0314JavaProect/src/day0327/StudentDto.java
+++ javawork/Naver0314JavaProect/src/day0327/StudentDto.java
@@ -8,15 +8,84 @@ public class StudentDto {
 	private int html;
 	
 	// 디폴트 생성자
-	
+	public StudentDto() {
+		super();
+	}
+		
 	// 매개변수 있는 생성자
-	
+	public StudentDto(String name, int age, int java, int spring, int html) {
+		super();
+		this.name = name;
+		this.age = age;
+		this.java = java;
+		this.spring = spring;
+		this.html = html;
+	}
 	// setter & getter
+
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public int getJava() {
+		return java;
+	}
+
+	public void setJava(int java) {
+		this.java = java;
+	}
+
+	public int getSpring() {
+		return spring;
+	}
+
+	public void setSpring(int spring) {
+		this.spring = spring;
+	}
+
+	public int getHtml() {
+		return html;
+	}
+
+	public void setHtml(int html) {
+		this.html = html;
+	}
 	
 	// java + spring + html의 합을 반환하는 메서드 (getTotal)
 	
+	public int getTotal() {
+		return getJava() + getSpring() + getHtml();
+		
+	}
+	
 	// getTotal()/3.0으로 나눠서 반환하는 메서드 (getAvg)
+	
+	public double getAvg() {
+		return getTotal() / 3.0;
+	}
 	
 	// getAvg의 값이 90이상이면 "*****", 80 이상이면 "****", 70 이상이면 "***", 나머지는 "*"
 	// 반환하는 메서드 (getGrade)
+	
+	public String getGrade() {
+		double score = getAvg();
+		
+		if(score >= 90) return "*****";
+		else if (score >= 80) return "****";
+		else if (score >= 70) return "***";
+		else return "*";
+	}
 }
